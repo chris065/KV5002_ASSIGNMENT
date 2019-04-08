5da4c4c2873687d75eedba16ad2e570d2a68ffbe
chrs065 <chrisbennett065@gmail.com>
2019-04-08 19:21:51 +0100

    added key input to get specific data, working on sending messages to the dashboard

--- Program/controller.c
+++ Program/controller.c
@@ -1,7 +1,7 @@
 /*
 Controller Class (C)
-Version: 1.1
-Version Date: 29/03/2019
+Version: 1.2
+Version Date: 08/04/2019
 Author: Chris Bennett (w17004754)
 */
 
@@ -19,13 +19,71 @@ Author: Chris Bennett (w17004754)
 #include <signal.h>
 #include <stdbool.h>
 
+const char *setCommand(char *command)
+{
+	if(strcmp(command, "1") == 0)
+	{
+		return "state:?";
+	}
+	if(strcmp(command, "2") == 0)
+	{
+		return "condition:?";
+	}
+	if(strcmp(command, "3") == 0)
+	{
+		return "terrain:?";
+	}
+	else
+	{
+		return "";
+	}
+}
+
+void finished(int sig)
+{
+    exit(0);
+}
+
+static int fd;
+void cleanup(void)
+{
+    close(fd);
+}
+
+
+int messageToDash(char *message[])
+{
+	char *port = "65250";
+	struct addrinfo *address;
+	const struct addrinfo hints = {.ai_flags = AI_PASSIVE, .ai_family = AF_INET, .ai_socktype = SOCK_DGRAM,};
+	int err;
+
+	atexit(cleanup);
+	signal(SIGINT, finished);
+	
+	err = getaddrinfo(NULL, port, &hints, &address);
+	if(err)
+	{
+		fprintf(stderr, "Error getting address: %s\n", gai_strerror(err));
+		exit(1);
+	}
+
+	fd = socket(AF_INET, SOCK_DGRAM, 0);
+	if(fd == -1)
+	{
+		fprintf(stderr, "Error making the socket: %s\n", strerror(errno));
+	}
+}
+
 
 int main(int argc, char *argv[])
 {
+	const char *command = setCommand(argv[1]);
+
 	//char *host = argv[1];
 	char *host = "127.0.1.1";
 	//char *port = argv[2];
-	char *port = "65200";
+	char *rcvPort = "65200";
 	
 	struct addrinfo *address;
 	const struct addrinfo hints =
@@ -35,7 +93,7 @@ int main(int argc, char *argv[])
 	};
 	int fd, err;
 	
-	err = getaddrinfo(host, port, &hints, &address);
+	err = getaddrinfo(host, rcvPort, &hints, &address);
 	if(err)
 	{
 		fprintf(stderr, "Error getting the address: %s\n", gai_strerror(err));
@@ -54,12 +112,16 @@ int main(int argc, char *argv[])
 	struct sockaddr clientaddr;
 	socklen_t addrlen = sizeof(clientaddr);
 	
-	strcpy(outgoing, argv[1]);
+	strcpy(outgoing, command);
 	sendto(fd, outgoing, strlen(outgoing), 0, address->ai_addr, address->ai_addrlen);
 
 	msgsize = recvfrom(fd, incoming, buffsize, 0, NULL, 0);
 	incoming[msgsize] = '\0';
 	printf("Reply: %s\n", incoming);
 }
+//mehtod to send message to the dash
+//take a paremeter that is the message
+
+
 
 
